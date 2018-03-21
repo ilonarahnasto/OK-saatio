280422c6977164c97702d00bd12b3da4d4a6d763
Ilona Rahnasto <ilonarahnasto@dhcp-85-252.eduroam.aalto.fi>
2018-03-21 17:49:47 +0200

    fixed navigation bar first version

--- static/js/main.js
+++ static/js/main.js
@@ -5,30 +5,33 @@ $(document).ready(function(){
 
     });
   }
+
   window.setTimeout(myFade, [800]);
 
-  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    }
   });
 
+$(window).scroll(function() {
+    var arkkitehdiksiheight = $('#static-nav').offset().top;
+    var currentheight = document.documentElement.scrollTop;
+    if (arkkitehdiksiheight < currentheight) {
+      $("#nav").fadeIn(300, function() {
+        console.log("is anything happening");
+      });
+    } else if (arkkitehdiksiheight > currentheight) {
+      $("#nav").fadeOut(300, function() {
+        console.log("something is happening");
+      });
+    }
+  });
 
 });
